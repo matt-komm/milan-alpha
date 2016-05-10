df3fe38562bb495b563e2c0b29dda234b5073180
matt-komm <mkomm@cern.ch>
2016-05-10 22:02:58 +0200

    reworked histograms

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ Tools for statistical inference and unfolding in HEP
 Build | Status
 ----- | ------
 Linux & OSX | [![Build Status](https://travis-ci.org/matt-komm/milan.svg?branch=master)](https://travis-ci.org/matt-komm/milan)
+
+#### Supported compilers
+* gcc >= 4.7 
+* clang >= 3.4 
+
--- core/include/milan/core/Histogram.hh
+++ core/include/milan/core/Histogram.hh
@@ -7,6 +7,9 @@
 
 #include <array>
 #include <cmath>
+#include <vector>
+#include <initializer_list>
+#include <iostream>
 
 namespace milan
 {
@@ -14,57 +17,65 @@ namespace milan
 template<class TYPE, sizetype DIM>
 class Histogram
 {
-    public:
-        typedef Binning<TYPE> B;
     protected:
-        std::array<Binning<TYPE>,DIM> _binning;
+        const std::vector<Binning<TYPE>> _binning;
         
         std::vector<TYPE> _content;
         std::vector<TYPE> _error2;
     public:
-        template<class... ARGS>
-        Histogram(ARGS&&... args):
-            _binning{{std::forward<ARGS>(args)...}}
-        {
-            static_assert(
-                DIM==sizeof...(args),
-                "Histogram needs to be initialized with same number of binning objects as there are dimensions"
-            );
+        Histogram(const std::vector<Binning<TYPE>>& binning):
+            _binning(binning)
+        {   
+            if (binning.size()!=DIM)
+            {
+                milan_throw("Histogram is of dimension ",DIM," but a binning scheme with ",binning.size()," was configured");
+            }
             sizetype N = 1;
             for (sizetype idim = 0; idim < DIM; ++idim)
             {
+                //add 2 to account for over- & underflow
                 N*=_binning[idim].size()+2;
             }
             _content = std::vector<TYPE>(N,0);
             _error2 = std::vector<TYPE>(N,0);
         }
         
-        inline TYPE getContent(const std::array<sizetype,DIM>& index) const
+        inline TYPE getContent(const std::vector<sizetype>& index) const
         {
             return _content[getGlobalBinFromIndex(index)];
         }
         
-        inline void setContent(const std::array<sizetype,DIM>& index, const TYPE& value)
+        inline void setContent(const std::vector<sizetype>& index, const TYPE& value)
         {
             _content[getGlobalBinFromIndex(index)] = value;
         }
         
-        inline TYPE getError(const std::array<sizetype,DIM>& index) const
+        inline TYPE getContent(const sizetype& globalIndex) const
+        {
+            return _content[globalIndex];
+        }
+        
+        inline void setContent(const sizetype& globalIndex, const TYPE& value)
+        {
+            _content[globalIndex] = value;
+        }
+        
+        inline TYPE getError(const std::vector<sizetype>& index) const
         {
             return std::sqrt(getError2(index));
         }
         
-        inline TYPE getError2(const std::array<sizetype,DIM>& index) const
+        inline TYPE getError2(const std::vector<sizetype>& index) const
         {
             return _error2[getGlobalBinFromIndex(index)];
         }
         
-        inline void setError(const std::array<sizetype,DIM>& index, const TYPE& error)
+        inline void setError(const std::vector<sizetype>& index, const TYPE& error)
         {
             setError2(index,error*error);
         }
         
-        inline void setError2(const std::array<sizetype,DIM>& index, const TYPE& error2)
+        inline void setError2(const std::vector<sizetype>& index, const TYPE& error2)
         {
             if (error2<0.0)
             {
@@ -78,10 +89,14 @@ class Histogram
             return _binning[idim];
         }
         
-        sizetype getGlobalBinFromIndex(const std::array<sizetype,DIM>& index) const
+        sizetype getGlobalBinFromIndex(const std::vector<sizetype>& index) const
         {
             sizetype globalIndex = 0;
             sizetype offset = 1;
+            if (index.size()!=DIM)
+            {
+                milan_throw("Index dimension ",index.size()," cannot be used to access histogram with dimension ",DIM);
+            }
             for (sizetype idim = 0; idim < DIM; ++idim)
             {
                 globalIndex+=index[idim]*offset;
@@ -90,46 +105,23 @@ class Histogram
             return globalIndex;
         }
         
-        inline std::array<sizetype,DIM> findIndexFromValue(const std::array<TYPE,DIM>& value) const
+        inline std::vector<sizetype> findIndexFromValue(const std::vector<TYPE>& value) const
         {
-            std::array<sizetype,DIM> index;
+            if (value.size()!=DIM)
+            {
+                milan_throw("Value dimension ",value.size()," cannot be used to fill histogram with dimension ",DIM);
+            }
+            std::vector<sizetype> index(DIM,0);
             for (sizetype idim = 0; idim < DIM; ++idim)
             {
                 index[idim]=_binning.findBin(value[idim]);
             }
-            return index;
+            return std::move(index);
         }
         
-        inline std::array<sizetype,DIM> findGlobalBinFromValue(const std::array<TYPE,DIM>& value) const
+        inline sizetype findGlobalBinFromValue(const std::vector<TYPE>& value) const
         {
             return getGlobalBinFromIndex(findIndexFromValue(value));
-        }
-};
-
-template<class TYPE>
-class Histogram1:
-    public Histogram<TYPE,1>
-{
-    public:
-        Histogram1(const Binning<TYPE>& binning):
-            Histogram<TYPE,1>({binning})
-        {
-        }
-        
-        inline TYPE getContent(const sizetype& indexX, const sizetype& indexY)
-        {
-            return this->getContent({indexX,indexY});
-        }
-};
-
-template<class TYPE>
-class Histogram2:
-    public Histogram<TYPE,2>
-{
-    public:
-        Histogram2(const Binning<TYPE>& binningX,const Binning<TYPE>& binningY):
-            Histogram<TYPE,2>({binningX,binningY})
-        {
         }
 };
 
