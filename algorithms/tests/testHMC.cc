f074719188eef98600382e6c124a2a1d6d703114
matt-komm <mkomm@cern.ch>
2016-11-04 21:52:40 +0100

    up hmc test

--- algorithms/tests/testHMC.cc
+++ algorithms/tests/testHMC.cc
@@ -27,9 +27,9 @@ TEST(HMC, construction)
     EXPECT_EQ(hmc.getParameters().size(),2);
 }
 
-class SimpleLL:
+class SimpleGaussianLL:
     public milan::LikelihoodInterface,
-    public milan::PtrInterface<milan::LikelihoodInterface,SimpleLL>
+    public milan::PtrInterface<milan::LikelihoodInterface,SimpleGaussianLL>
 {
     protected:
         milan::Ptr<milan::Parameter> _p;
@@ -38,7 +38,7 @@ class SimpleLL:
         std::vector<milan::Ptr<milan::Parameter>> _lagrangeParameters;
     public:
     
-        SimpleLL(const milan::Ptr<milan::Parameter>& p, double mean=0, double std=1): 
+        SimpleGaussianLL(const milan::Ptr<milan::Parameter>& p, double mean=0, double std=1): 
             _p(p),
             _mean(mean),
             _std(std),
@@ -53,7 +53,7 @@ class SimpleLL:
         }
         virtual double getNLL() const
         {
-            return 0.5*(_p->getValue()-_mean)*(_p->getValue()-_mean)/_std/_std; //Gaussian with mean=1.5 and std=2
+            return 0.5*(_p->getValue()-_mean)*(_p->getValue()-_mean)/_std/_std; //Gaussian with mean=p
         }
         virtual std::vector<double> getNLLValueAndDerivatives(const std::vector<milan::Ptr<milan::Parameter>>& parameters) const
         {
@@ -77,13 +77,62 @@ class SimpleLL:
         
 };
 
+class SimplePoissonLL:
+    public milan::LikelihoodInterface,
+    public milan::PtrInterface<milan::LikelihoodInterface,SimplePoissonLL>
+{
+    protected:
+        milan::Ptr<milan::Parameter> _p;
+        double _obs;
+        std::vector<milan::Ptr<milan::Parameter>> _lagrangeParameters;
+    public:
+    
+        SimplePoissonLL(const milan::Ptr<milan::Parameter>& p, double obs=1): 
+            _p(p),
+            _obs(obs),
+            _lagrangeParameters(0) 
+        {
+        }
+        
+        virtual double getNLLDerivative(const milan::Ptr<milan::Parameter>& parameter) const
+        {
+            if (*_p == *parameter) return -_obs/_p->getValue()+1;
+            return 0;
+        }
+        virtual double getNLL() const
+        {
+            return -_obs*vdt::fast_log(_p->getValue())+_p->getValue(); //Poisson with mean=p
+        }
+        virtual std::vector<double> getNLLValueAndDerivatives(const std::vector<milan::Ptr<milan::Parameter>>& parameters) const
+        {
+            std::vector<double> result(parameters.size()+1,0);
+            result[0] = getNLL();
+            for (milan::sizetype i = 0; i < parameters.size(); ++i) result[i+1] = getNLLDerivative(parameters[i]);
+            return result;
+        }
+        
+        //returns pointers to potential lagrange parameters (e.g. for Barlow-Beeston) for additional minimizations
+        virtual const std::vector<milan::Ptr<milan::Parameter>>& getLagrangeParameters() const
+        {
+            return _lagrangeParameters;
+
+        }
+        
+        operator milan::Likelihood()
+        {
+            return copy();
+        }
+        
+};
+
+
 TEST(HMC, speed)
 {
     using namespace milan;
     
     const sizetype N = 1;
     Histogram data({Binning(N,-1,1)});
-    for (unsigned int i = 0; i < N; ++i) data.setContent(i+1,1);
+    for (unsigned int i = 0; i < N; ++i) data.setContent(i+1,3);
     HistogramFunction dataFct(data.ref());
     
     Parameter signalYield("mu",1.0,Parameter::MIN,Parameter::MAX,0.2);
@@ -94,12 +143,20 @@ TEST(HMC, speed)
     
     BinnedLikelihood bll(dataFct,signalPrediction);
     
-    //Likelihood ll = SimpleLL(signalYield,1.3,0.3);
-    Likelihood ll = bll.copy();
-    for (sizetype i = 0; i < 21; ++i)
+    //Likelihood ll = SimpleGaussianLL(signalYield,-3.0,2.0);
+    Likelihood ll = SimplePoissonLL(signalYield,24.0);
+    //Likelihood ll = bll.copy();
+    for (sizetype i = 0; i < 41; ++i)
     {
-        signalYield.setValue(0.0+0.1*i);
-        std::cout<<"nll(s="<<signalYield.getValue()<<")="<<ll.getNLL()<<", d1="<<data.getContent(1)<<", s1="<<signalTemplate.getContent(1)<<", p1="<<signalPrediction.getContent(1)<<std::endl;
+        signalYield.setValue(0.0+0.2*i);
+        printf(
+            "nll(s=%2.1f) = %+5.3f\n",// | d=%3.1f | s=%3.1f | p=%3.1f\n",
+            signalYield.getValue(),
+            ll.getNLL()/*,
+            data.getContent(1),
+            signalTemplate.getContent(1),
+            signalPrediction.getContent(1)*/
+        );
     }
     
     HMC hmc;
@@ -107,7 +164,7 @@ TEST(HMC, speed)
     std::vector<Ptr<Parameter>> bbParameters = ll.getLagrangeParameters();
     //for (unsigned int i = 0; i < bbParameters.size(); ++i) hmc.addParameter(bbParameters[i]);
     const sizetype BURN = 1000;
-    const sizetype TOYS = 10000;
+    const sizetype TOYS = 100000;
     int accepted = 0;
     double mean = 0.0;
     double mean2 = 0.0;
