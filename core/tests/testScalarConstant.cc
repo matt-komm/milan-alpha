c9b1aabbfb1baf992a0a48bcaece752bae160d7f
matt-komm <mkomm@cern.ch>
2016-11-08 15:21:47 +0100

    up

--- core/tests/testScalarConstant.cc
+++ core/tests/testScalarConstant.cc
@@ -2,28 +2,119 @@
 #include "milan/core/Parameter.hh"
 #include "gtest/gtest.h"
 
+#include "test_macros.h"
+
 TEST(ScalarConstant, construction)
 {
     using namespace milan;
-    ScalarConstant x = 5;
-    Parameter fakeParameter("fake");
-    
-    EXPECT_DOUBLE_EQ(x.getValue(),5);
-    EXPECT_DOUBLE_EQ(x.getDifferential(fakeParameter),0);
-    EXPECT_DOUBLE_EQ(x.getValueAndDerivatives({})[0],5);
-    EXPECT_DOUBLE_EQ(x.getValueAndDerivatives({fakeParameter})[0],5);
-    EXPECT_DOUBLE_EQ(x.getValueAndDerivatives({fakeParameter})[1],0);
+    for (sizetype i = 0; i <10; ++i)
+    {
+        ScalarConstant x = 0.5*i-2;
+        Parameter fakeParameter("fake");
+        
+        EXPECT_DOUBLE_EQ(x.getValue(),0.5*i-2);
+        EXPECT_DOUBLE_EQ(x.getDifferential(fakeParameter),0);
+        EXPECT_DOUBLE_EQ(x.getValueAndDerivatives({})[0],0.5*i-2);
+        EXPECT_DOUBLE_EQ(x.getValueAndDerivatives({fakeParameter})[0],0.5*i-2);
+        EXPECT_DOUBLE_EQ(x.getValueAndDerivatives({fakeParameter})[1],0);
+    }
     
 }
-TEST(ScalarConstant, operators)
+
+TEST(ScalarConstant, add)
 {
     using namespace milan;
-    ScalarConstant x = 5;
-    Parameter fakeParameter("fake");
-    x+=10;
-    EXPECT_DOUBLE_EQ(x.getValue(),15);
-    EXPECT_DOUBLE_EQ(x.getDifferential(fakeParameter),0);
-    EXPECT_DOUBLE_EQ(x.getValueAndDerivatives({})[0],15);
-    EXPECT_DOUBLE_EQ(x.getValueAndDerivatives({fakeParameter})[0],15);
-    EXPECT_DOUBLE_EQ(x.getValueAndDerivatives({fakeParameter})[1],0);
+    for (sizetype i = 0; i <10; ++i)
+    {
+        for (sizetype j = 0; j <10; ++j)
+        {
+            
+            const double a = 0.5*i-2;
+            const double b = 0.11*j-0.05;
+            const double result = a+b;
+            
+            ScalarConstant x = a;
+            x+=b;
+            Parameter fakeParameter("fake");
+            
+            EXPECT_DOUBLE_EQ(x.getValue(),result);
+            EXPECT_DOUBLE_EQ(x.getDifferential(fakeParameter),0);
+            EXPECT_DOUBLE_EQ(x.getValueAndDerivatives({})[0],result);
+            EXPECT_DOUBLE_EQ(x.getValueAndDerivatives({fakeParameter})[0],result);
+            EXPECT_DOUBLE_EQ(x.getValueAndDerivatives({fakeParameter})[1],0);
+        }
+    }
+}
+
+TEST(ScalarConstant, sub)
+{
+    using namespace milan;
+    for (sizetype i = 0; i <10; ++i)
+    {
+        for (sizetype j = 0; j <10; ++j)
+        {
+            const double a = 0.5*i-2;
+            const double b = 0.11*j-0.05;
+            const double result = a-b;
+            
+            ScalarConstant x = a;
+            x-=b;
+            Parameter fakeParameter("fake");
+            
+            EXPECT_DOUBLE_EQ(x.getValue(),result);
+            EXPECT_DOUBLE_EQ(x.getDifferential(fakeParameter),0);
+            EXPECT_DOUBLE_EQ(x.getValueAndDerivatives({})[0],result);
+            EXPECT_DOUBLE_EQ(x.getValueAndDerivatives({fakeParameter})[0],result);
+            EXPECT_DOUBLE_EQ(x.getValueAndDerivatives({fakeParameter})[1],0);
+        }
+    }
+}
+
+TEST(ScalarConstant, mul)
+{
+    using namespace milan;
+    for (sizetype i = 0; i <10; ++i)
+    {
+        for (sizetype j = 0; j <10; ++j)
+        {
+            const double a = 0.5*i-2;
+            const double b = 0.11*j-0.05;
+            const double result = a*b;
+            
+            ScalarConstant x = a;
+            x*=b;
+            Parameter fakeParameter("fake");
+            
+            EXPECT_APPROX_FLOAT_EQ(x.getValue(),result);
+            EXPECT_DOUBLE_EQ(x.getDifferential(fakeParameter),0);
+            EXPECT_APPROX_FLOAT_EQ(x.getValueAndDerivatives({})[0],result);
+            EXPECT_APPROX_FLOAT_EQ(x.getValueAndDerivatives({fakeParameter})[0],result);
+            EXPECT_DOUBLE_EQ(x.getValueAndDerivatives({fakeParameter})[1],0);
+        }
+    }
+}
+
+
+TEST(ScalarConstant, div)
+{
+    using namespace milan;
+    for (sizetype i = 0; i <10; ++i)
+    {
+        for (sizetype j = 0; j <10; ++j)
+        {
+            const double a = 0.5*i-2;
+            const double b = 0.11*j-0.05;
+            const double result = a/b;
+            
+            ScalarConstant x = a;
+            x/=b;
+            Parameter fakeParameter("fake");
+            
+            EXPECT_DOUBLE_EQ(x.getValue(),result);
+            EXPECT_DOUBLE_EQ(x.getDifferential(fakeParameter),0);
+            EXPECT_DOUBLE_EQ(x.getValueAndDerivatives({})[0],result);
+            EXPECT_DOUBLE_EQ(x.getValueAndDerivatives({fakeParameter})[0],result);
+            EXPECT_DOUBLE_EQ(x.getValueAndDerivatives({fakeParameter})[1],0);
+        }
+    }
 }
---
+++ tests/test_macros.h
@@ -0,0 +1,40 @@
+#ifndef __MILAN_TEST_MACROS_H__
+#define __MILAN_TEST_MACROS_H__
+
+#include "gtest/gtest.h"
+
+#include <limits>
+
+namespace milan
+{
+
+template<class T> inline bool approximatelyEqual(const T& a, const T& b, const T& epsilon=std::numeric_limits<T>::epsilon())
+{
+    return fabs(a - b) <= ( (std::fabs(a) < std::fabs(b) ? std::fabs(b) : std::fabs(a)) * epsilon);
+}
+
+template<class T> inline bool essentiallyEqual(const T& a, const T& b, const T& epsilon=std::numeric_limits<T>::epsilon())
+{
+    return fabs(a - b) <= ( (std::fabs(a) > std::fabs(b) ? std::fabs(b) : std::fabs(a)) * epsilon);
+}
+
+template<class T> inline bool definitelyGreaterThan(const T& a, const T& b, const T& epsilon=std::numeric_limits<T>::epsilon())
+{
+    return (a - b) > ( (std::fabs(a) < std::fabs(b) ? std::fabs(b) : std::fabs(a)) * epsilon);
+}
+
+template<class T> inline bool definitelyLessThan(const T& a, const T& b, const T& epsilon=std::numeric_limits<T>::epsilon())
+{
+    return (b - a) > ( (std::fabs(a) < std::fabs(b) ? std::fabs(b) : std::fabs(a)) * epsilon);
+}
+
+}
+
+#endif
+
+#define EXPECT_APPROX_EQ(a,b) \
+    EXPECT_EQ(milan::approximatelyEqual(a,b),true) <<"expected "<<#a<<" which is "<<a<<" to be equal to "<<#b<<" which is "<<b;
+
+#define EXPECT_APPROX_FLOAT_EQ(a,b) \
+    EXPECT_EQ(milan::approximatelyEqual<float>(a,b),true) <<"expected "<<#a<<" which is "<<a<<" to be equal to "<<#b<<" which is "<<b;
+
